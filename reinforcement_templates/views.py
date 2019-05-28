d65a4c264d440e5f963bba53ef75d633f26a098e
hassanashmawy <hassan.ashmawy@gmail.com>
2019-05-28 13:53:35 -0400

    done

--- reinforcement_templates/views.py
+++ reinforcement_templates/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def new_profile(request):
-    context = {}
+    ls_profile = ['email', 'username', 'pin', 'website', 'address', 'alias']
+    context = {'ls_profile': ls_profile}
     return HttpResponse(render(request, 'profiles/new.html', context))
